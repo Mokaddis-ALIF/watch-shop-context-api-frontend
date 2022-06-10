0a3d10403d1d3e0a47d45e719eda7c44ba7f2ef0
Alif-07 <mokaddis.alif@gmail.com>
2022-06-11 00:10:59 +0600

    fetch code

--- src/shared/Home.js
+++ src/shared/Home.js
@@ -1,21 +1,54 @@
-import React, { useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import './Home.css';
 import Featured from '../components/Featured/Featured';
+import Footer from '../components/Footer/Footer';
 import HomeBanner from '../components/HomeBanner/HomeBanner';
+import NewProducts from '../components/NewProducts/NewProducts';
 import NewsLetter from '../components/NewsLetter/NewsLetter';
 import Products from '../components/Products/Products';
 import Story from '../components/Story/Story';
-import Footer from '../components/Footer/Footer';
-import NewProducts from '../components/NewProducts/NewProducts';
 
 const Home = ({ closeToggleSidebar, close, sidebar }) => {
 	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState();
 
 	useEffect(() => {
-		fetch(`https://thawing-eyrie-47965.herokuapp.com/products`)
-			.then((res) => res.json())
-			.then((data) => setProducts(data));
+		const fetchProducts = async () => {
+			const response = await fetch(
+				`https://thawing-eyrie-47965.herokuapp.com/products`
+			);
+
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+
+			const data = await response.json();
+			setProducts(data);
+			setIsLoading(false);
+		};
+
+		fetchProducts().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error.message);
+		});
 	}, []);
+
+	if (isLoading) {
+		return (
+			<section className="MealsLoading">
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<section className="MealsError">
+				<p>{httpError}</p>
+			</section>
+		);
+	}
 
 	return (
 		<>
